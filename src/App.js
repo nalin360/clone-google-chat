da27fa5e0f7265ff9253d8464b78180871c4ea28
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-08-18 23:14:47 +0530

    main pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { auth } from './config/firebase';
+import ChatRoom from './pages/ChatRoom';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 
 function App() {
+  const [hiuser, setHiUser] = useState(null)
+  const [user,loading] = useAuthState(auth);
+  useEffect(() => {
+    if (user) {
+      setHiUser(user.emailVerified);
+    }
+  }, [user]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
+      </header>
+
+      <section>
+        {hiuser ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
