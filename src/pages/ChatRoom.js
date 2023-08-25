e46e82ded5229b86e76591f02ce979ce098b7f27
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-08-25 22:40:17 +0530

    added Header Main Welcom components

--- src/pages/ChatRoom.js
+++ src/pages/ChatRoom.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import {useState} from 'react'
+import HeaderMain from '../components/header'
+import MainMenu from '../components/MainMenu'
+import WelcomPages from '../components/MainRooms/WelcomPages'
 
 function ChatRoom() {
+  const [toogleSideMEnu, settoogleSideMEnu] = useState(false)
   return (
-    <div>ChatRoom</div>
+    <div>
+      <HeaderMain toogeSideMenuCallback={settoogleSideMEnu}/>
+      <MainMenu effect={toogleSideMEnu}/>
+      <WelcomPages effect={toogleSideMEnu}/>
+    </div>
   )
 }
 
