e66d1eeed521a3ba320c8cef05878730ff7258f5
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-08-25 22:42:12 +0530

    header components

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import {useState} from 'react'
 
-function header() {
+function HeaderMain({toogeSideMenuCallback}) {
+  const [clickToogleSidebarCount, setclickToogleSidebarCount] = useState(0)
+  const handleMenuClick = () =>{
+    // TODO try accesing effects to solve error 
+    if (clickToogleSidebarCount === 1) {
+      setclickToogleSidebarCount(0)
+      return false
+    } 
+    setclickToogleSidebarCount(1)
+    return true
+
+  }
   return (
-    <div>header</div>
+    <div>
+      <div>
+        <button onClick={() => toogeSideMenuCallback(handleMenuClick)}>M-B</button>
+        
+      </div>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+
+    </div>
   )
 }
 
-export default header
+export default HeaderMain
